45d28decf0fe859fd9940563d7a17ae1917c767a
liuyuzhao <liuyuzhao@xyz10.com>
2024-08-16 16:40:37 +0800

    osc52

---
+++ lua/clipboard.lua
@@ -0,0 +1,15 @@
+-- If using Neovim under SSH, using OSC52 to synchronous system clipboard.
+vim.opt.clipboard:append "unnamedplus"
+if vim.fn.exists "$SSH_TTY" == 1 and vim.env.TMUX == nil then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy "+",
+      ["*"] = require("vim.ui.clipboard.osc52").copy "*",
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste "+",
+      ["*"] = require("vim.ui.clipboard.osc52").paste "*",
+    },
+  }
+end
