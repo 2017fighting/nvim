864d569a5fc2748ba1915d84890ddc03af034d86
Ryan Valor <272567571@qq.com>
2026-05-13 15:40:34 +0800

    feat(community): add nvim-surround

--- lua/community.lua
+++ lua/community.lua
@@ -9,6 +9,8 @@ return {
   -- import/override with your plugins folder
   { import = "astrocommunity.colorscheme.catppuccin" },
 
+  { import = "astrocommunity.motion.nvim-surround" },
+
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.helm" },
   { import = "astrocommunity.pack.html-css" },
