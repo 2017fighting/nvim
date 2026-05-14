3bd2c96500b1256de45149b9dff909e9b1ce49fd
Ryan Valor <272567571@qq.com>
2026-05-14 17:03:50 +0800

    feat(core): <C-q> is a replace of <C-v> in windows, so unset it.

---
+++ lua/rewrite.lua
@@ -0,0 +1,13 @@
+---@type LazySpec
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    mappings = {
+      -- NORMAL MODE
+      n = {
+        ["<C-q>"] = false,
+      },
+    },
+  },
+}
